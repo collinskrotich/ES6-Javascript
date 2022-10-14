077e7f423558d2433673afb970e60d578f957f15
Collins Kiprono Rotich <kolinsr@gmail.com>
2022-10-14 08:26:15 +0300

    promises in js

--- exportdefault.js
+++ exportdefault.js
@@ -16,9 +16,8 @@ The first is a named function, and the second is an anonymous function.
 
 Since export default is used to declare a fallback value for a module or file, you can only have one value be a default export in each module or file. Additionally, you cannot use export default with var, let, or const
 
-The following function should be the fallback value for the module. Please add the necessary code to do so.
+// The following function should be the fallback value for the module. Please add the necessary code to do so.
 
 export default function subtract(x, y) {
     return x - y;
   }
-  
